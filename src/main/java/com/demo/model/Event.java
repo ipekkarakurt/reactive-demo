b840603e554f841e038df24221b39ab6ffe3782d
İpek Karakurt <ipekkarakurt@yandex.com>
2018-12-28 00:18:49 +0300

    database config

--- src/main/java/com/demo/model/Event.java
+++ src/main/java/com/demo/model/Event.java
@@ -2,11 +2,16 @@ package com.demo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.Date;
 
 import java.util.Date;
 
 @Data
+@Document(collection = "events")
 public class Event {
+    @Id
     private long id;
     private Date date;
 
