c33ab11dcb66e4ba5007fabe855e0ba7a59cc600
İpek Karakurt <ipekkarakurt@yandex.com>
2018-12-28 00:25:52 +0300

    add reactive mongo repository

--- src/main/java/com/demo/repository/EventRepository.java
+++ src/main/java/com/demo/repository/EventRepository.java
@@ -1,4 +1,10 @@
 package com.demo.repository;
 
-public class EventRepository {
+import com.demo.model.Event;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends ReactiveMongoRepository <Event, String> {
+
 }
